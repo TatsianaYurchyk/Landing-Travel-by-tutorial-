9c9aacd042b999b8f7bf8397253099c7ccc5bb50
TatsianaYurchik <t26yurchik@gmail.com>
2022-06-11 17:06:54 +0300

    change window.scrollY meaning

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 (function() {
     const header = document.querySelector(".header");
     window.onscroll = () => {
-        if (window.scrollY >10){
+        if (window.scrollY >20){
             header.classList.add ("header_scroll");
         } else {
             header.classList.remove ("header_scroll")
