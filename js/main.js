d66af4ba82da20cbd5236818f214a260d9486eb7
TatsianaYurchyk <t26yurchik@gmail.com>
2022-06-25 11:08:53 +0300

    add adaptive menu

--- js/main.js
+++ js/main.js
@@ -7,5 +7,17 @@
         } else {
             header.classList.remove ("header_scroll")
         }
-    }
-}())
+    };
+}());
+(function(){
+    const burgerThumb = document.querySelector(".burger");
+    const menu = document.querySelector(".header_nav");
+    const burgerClose = document.querySelector(".burger_close");
+    burgerThumb.addEventListener('click', () => {
+        menu.classList.add("header_nav_active");
+    });
+    burgerClose.addEventListener('click', () => {
+        menu.classList.remove("header_nav_active")
+    });
+
+}());
